0589e6f0928d0874a7a773e459f6d06e7cf2be83
Purushottam Nawale <96472310+purushottamnawale@users.noreply.github.com>
2023-06-27 17:54:45 +0530

    Update settings.py

--- employee_project/employee_project/settings.py
+++ employee_project/employee_project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'employee_register',
-    'crispy_forms'
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
